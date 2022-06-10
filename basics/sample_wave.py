c192708b311b105c0484788690437069c66952e0
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-10 21:03:52 +0800

    Add: duplicate file

--- basics/sample_wave.py
+++ basics/sample_wave.py
@@ -13,3 +13,18 @@ print("Sample width", obj.getsampwidth())
 print("Framerate", obj.getframerate())
 print("No. of frames", obj.getnframes())
 print("Parameters", obj.getparams())
+
+audio_time = obj.getnframes() / obj.getframerate()
+print(audio_time)
+
+frames = obj.readframes(-1)
+print(type(frames), type(frames[0]))
+print(len(frames) / 2)
+obj.close()
+
+obj_new = wave.open("new_sample.wav", "wb")
+obj_new.setnchannels(1)
+obj_new.setsampwidth(2)
+obj_new.setframerate(48000)
+obj_new.writeframes(frames)
+obj_new.close()
