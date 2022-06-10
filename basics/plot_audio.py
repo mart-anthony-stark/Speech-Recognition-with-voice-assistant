93b8992627621f45e07cb3eb81a3c39dfaf70d0a
mart-anthony-stark <martsalazar0@gmail.com>
2022-06-10 21:24:02 +0800

    Add: plotting audio signals

--- basics/plot_audio.py
+++ basics/plot_audio.py
@@ -12,4 +12,16 @@ signal_wave = obj.readframes(-1)
 obj.close()
 
 audio_time = n_samples / sample_freq
-print(audio_time)
+print(audio_time, "seconds")
+
+signal_array = np.frombuffer(signal_wave, dtype=np.int16)
+
+times = np.linspace(0, audio_time, num=n_samples)
+
+plt.figure(figsize=(15, 5))
+plt.plot(times, signal_array)
+plt.title("Audio Signal")
+plt.xlabel("Time in seconds")
+plt.ylabel("Signal Wave")
+plt.xlim(0, audio_time)
+plt.show()
